270d137714bf2db95b9d1cbb90fb4c77f143690e
Irina_Mos <imosunova@mail.ru>
2025-10-30 14:09:30 +0500

    Python_DZ_Modul__02_Operatory_vetvlenij_Part_2

---
+++ Python_Modul02_Operatory_vetvlenij_Part_2/Exercise_3.py
@@ -0,0 +1,7 @@
+x = int(input('Enter number: '))
+if x > 0:
+    print('Number is positive')
+elif x < 0:
+    print('Number is negative')
+else:
+    print('Number is equal to zero')
---
+++ Python_Modul02_Operatory_vetvlenij_Part_2/Exercise_4.py
@@ -0,0 +1,9 @@
+x = int(input('Enter number 1: '))
+y = int(input('Enter number 2: '))
+if x == y:
+    print('The number are equal')
+else:
+    if x > y:
+        print(y, x)
+    else:
+        print(x, y)
